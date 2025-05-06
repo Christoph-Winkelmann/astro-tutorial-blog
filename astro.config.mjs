484d11c18c7b727b023167af17c7886728d582c1
Christoph-Winkelmann <christoph.winkelmann@live.de>
2025-05-06 16:12:26 +0200

    added light and dark mode styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cw-astro-tutorial-blog.netlify.app/"
+  site: "https://cw-astro-tutorial-blog.netlify.app/",
+  integrations: [preact()]
 });
